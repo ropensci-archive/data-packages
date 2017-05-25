ac9f027eb3adb7f9f9faac33649303ef90a54139
ateucher <andy.teucher@gmail.com>
2017-05-25 13:38:44 -0700

    Create raw.githubusercontent url for rd file

--- R/get_metacran.R
+++ R/get_metacran.R
@@ -5,11 +5,13 @@ lapply(res[[3]], function(x) {
   reponame <- x$repository$name
   repo_url <- paste0("https://github.com/cran/", reponame)
   dataset_rd <- gsub("\\.Rd$", "", x$name)
+  dataset_rd_url <- gsub("github\\.com", "raw.githubusercontent.com", 
+                         x$html_url)
+  dataset_rd_url <- gsub("/blob/", "/", dataset_rd_url)
   
   list(reponame = reponame, 
        repo_url = repo_url, 
        dataset_rd = dataset_rd, 
-       dataset_rd_url = paste("https://raw.githubusercontent.com/cran", 
-                              reponame, x$sha, x$path, sep = "/"))
+       dataset_rd_url = dataset_rd_url)
 }
 )
