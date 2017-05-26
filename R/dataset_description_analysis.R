9092d6b10a27beb08e357393c01fcffd5e2a553a
jn <tupiszakaczki@gmail.com>
2017-05-26 11:55:59 -0700

    plot

--- R/dataset_description_analysis.R
+++ R/dataset_description_analysis.R
@@ -1,3 +1,4 @@
+library(forcats)
 library(tidyverse)
 
 data_cran <- read_rds("data/description_metadata.rds")
@@ -22,3 +23,20 @@ ggplot(data_cran, aes(BugReports_is_na)) +
 ggplot(data_cran, aes(URL_is_na)) +
   geom_bar() +
   facet_wrap(~is_data_package)
+
+df <- data_cran %>% 
+  select(is_data_package, License) %>% 
+  group_by(is_data_package, License) %>% 
+  summarise(n=n())
+
+df_wide <- df %>% 
+  spread(is_data_package, n) %>% 
+  mutate(`FALSE` = ifelse(is.na(`FALSE`), 0, `FALSE`),
+         `TRUE` = ifelse(is.na(`TRUE`), 0, `TRUE`)) %>% 
+  mutate(diff=`TRUE`-`FALSE`)
+
+ggplot(df_wide, aes(fct_reorder(License, diff), diff)) +
+  geom_col() + 
+  coord_flip() +
+  labs(x="License", y="<--- No data folder ----------- Data folder --->")
+ 
