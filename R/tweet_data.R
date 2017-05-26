3ae20c0d3fc748f07376435b38a0b4b2d04ed961
jsta <jstachelek@utexas.edu>
2017-05-26 10:15:02 -0700

    lower case rstats tag

--- R/tweet_data.R
+++ R/tweet_data.R
@@ -9,6 +9,6 @@ dt <- read.csv("https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/m
 i <- sample(seq_len(nrow(dt)), 1L)
 cran_link <- paste0("https://cran.r-project.org/package=", 
                     dt[i, "Package"])
-toot <- paste0("#RData: ", dt[i, "Title"], " - ", cran_link)
+toot <- paste0("#rdata #rstats: ", dt[i, "Title"], " - ", cran_link)
 
 post_tweet(toot, token = token)
