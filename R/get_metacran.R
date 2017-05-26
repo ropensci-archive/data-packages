4c0c591f9a1995dd88f5b9215776391c5731579e
ateucher <andy.teucher@gmail.com>
2017-05-26 12:38:43 -0700

    Parse all Rd files, not just first in each package

--- R/get_metacran.R
+++ R/get_metacran.R
@@ -45,35 +45,38 @@ saveRDS(pkgs_data_rds, saved_gh_data_rd_files)
 
 
 ## Download and parse the Rd files
-rd_file_meta <- lapply(pkgs_data_rds_saved, function(x) {
-  if (!length(x$items)) return(NULL)
-  x <- x$items[[1]]
-  reponame <- x$repository$name
-  repo_url <- paste0("https://github.com/cran/", reponame)
-  dataset_rd <- gsub("\\.Rd$", "", x$name)
-  dataset_rd_url <- gsub("github\\.com", "raw.githubusercontent.com", x$html_url)
-  dataset_rd_url <- gsub("/blob/", "/", dataset_rd_url)
-  rd_text <- readLines(dataset_rd_url)
-  list(name = reponame, 
-       repo_url = repo_url, 
-       dataset_rd_url = dataset_rd_url, 
-       rd_text = rd_text,
-       rd_text_parsed = tools::parse_Rd(textConnection(rd_text))
-  )
-})
+rd_file_meta <- lapply(pkgs_data_rds_saved, function(a) {
+  if (!length(a$items)) return(NULL)
+  lapply(a$items, function(x) {
+    reponame <- x$repository$name
+    repo_url <- paste0("https://github.com/cran/", reponame)
+    dataset_rd <- gsub("\\.Rd$", "", x$name)
+    dataset_rd_url <- gsub("github\\.com", "raw.githubusercontent.com", x$html_url)
+    dataset_rd_url <- gsub("/blob/", "/", dataset_rd_url)
+    rd_text <- readr::read_lines(dataset_rd_url)
+    list(name = reponame, 
+         repo_url = repo_url, 
+         dataset_rd_url = dataset_rd_url, 
+         rd_text = rd_text,
+         rd_text_parsed = tools::parse_Rd(textConnection(rd_text))
+    )
+  })
+}) %>% 
+  flatten()
 
 ## Combine parsed Rd file info into a data frame
 rd_metadata <- map_df(rd_file_meta, function(x) {
   if (is.null(x)) return(NULL)
   data_frame(pkg_name = x$name, 
              dataset_name = get_tag(x$rd_text_parsed, "name"), 
-             dataset_alias = get_tag(x$rd_text_parsed, "alias"),
+             # dataset_alias = get_tag(x$rd_text_parsed, "alias"), # May be better to not include this as it is sometimes not used appropriately
              dataset_title = paste(get_tag(x$rd_text_parsed, "title"), collapse = " "), 
              dataset_description = paste(get_tag(x$rd_text_parsed, "description"), collapse = " "), 
-             dateset_format = paste(get_tag(x$rd_text_parsed, "format"), collapse = " "),
-             dateset_source = paste(get_tag(x$rd_text_parsed, "source"), collapse = " "),
-             dateset_reference = paste(get_tag(x$rd_text_parsed, "references"), collapse = " "),
-             datset_has_examples = ifelse(all(is.na(get_tag(x$rd_text_parsed, "examples"))), FALSE, TRUE)
+             dataset_format = paste(get_tag(x$rd_text_parsed, "format"), collapse = " "),
+             dataset_source = paste(get_tag(x$rd_text_parsed, "source"), collapse = " "),
+             dataset_reference = paste(get_tag(x$rd_text_parsed, "references"), collapse = " "),
+             dataset_has_examples = ifelse(all(is.na(get_tag(x$rd_text_parsed, "examples"))), FALSE, TRUE), 
+             dataset_has_links = any(grepl("\\\\code\\{\\\\link", x$rd_text))
   )
 })
 
